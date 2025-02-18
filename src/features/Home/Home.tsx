c8cc8e07d22a3546476ed87c233e3edd228c0952
udoy <udoyrahman983@gmail.com>
2025-02-18 17:08:19 +0600

    refactor: update Home component layout and video handling for improved responsiveness

--- src/features/Home/Home.tsx
+++ src/features/Home/Home.tsx
@@ -5,18 +5,43 @@ function Home() {
     <>
       <Navbar />
       <main>
-        <section className="min-h-screen bg-gray-900">
-          <div className="absolute top-0 left-0 w-screen h-screen flex flex-col justify-center items-center">
-            <video className="absolute top-0 left-0 w-full h-full object-cover" autoPlay loop muted>
-              <source src="./src/assets/background_video.mp4" type="video/mp4" />
-              Your browser does not support the video tag.
-            </video>
-            <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50"></div>
+        <video
+          className="fixed top-0 left-0 h-full w-full object-cover"
+          autoPlay
+          loop
+          muted
+        >
+          <source src="./src/assets/background_video.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <div className="fixed top-0 left-0 h-full w-full bg-black opacity-50"></div>
+        <section className="relative z-10 flex min-h-screen items-center justify-center">
+          <div className="text-center">
             <img
-              className="relative h-100 object-contain"
+              className="relative mb-4 h-100 object-contain"
               src="./src/assets/GameLogo.png"
             />
-            <h1 className="relative text-white markazi-text-600 text-5xl">February 28, 2025</h1>
+            <h1 className="markazi-text-600 relative text-5xl text-white">
+              February 28, 2025
+            </h1>
+          </div>
+
+          {/* latest trailer */}
+        </section>
+        <section className="relative z-10 min-h-screen w-screen">
+            <div className="relative z-10 mx-auto aspect-video max-w-[70%] bg-[#565656] border-6 border-orange-50 filter drop-shadow-[0_0_20px_rgba(255,237,212,0.8)]">
+            <video
+              className="h-full w-full object-cover"
+              autoPlay
+              loop
+              muted
+            >
+              <source
+                src="./src/assets/background_video.mp4"
+                type="video/mp4"
+              />
+              Your browser does not support the video tag.
+            </video>
           </div>
         </section>
       </main>
