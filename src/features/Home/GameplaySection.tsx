e436b796da07312e08ab36ff13b54b2c3e84f92d
udoy <udoyrahman983@gmail.com>
2025-03-20 22:01:17 +0600

    feat: update GameplaySection text styling and content for improved narrative engagement

--- src/features/Home/GameplaySection.tsx
+++ src/features/Home/GameplaySection.tsx
@@ -30,12 +30,12 @@ function GameplaySection() {
             <br />
             <br />
             <br />
-            <span className="markazi-text-600 text-4xl/9 text-white">
+            <span className="markazi-text-600 text-4xl/9 text-white" style={{fontStyle: 'italic'}}>
             Our project has 2 playable characters, depending on the story progression we would switch the player back and forth to these characters to keep the story fresh and engaging with different perspectives of the narrative progress.
             </span>
             <br />
             <br />
-            <span className="markazi-text-500 text-2xl text-slate-300 " style={{fontStyle: 'italic'}}>
+            <span className="markazi-text-500 text-2xl text-slate-300 " >
               Explore the Level Based world of Luneria. Face against formidable Foes and allies alike. Gather and Craft resources for your survival and learn secrets of your adversaries. Secrets, that might make them hesitant to point their swords at you or make them vulnerable during combat. Remember, Battles are won best when you don't even have to draw your weapon unless of course, you are forced to do so. Escape the enemies grasp taking advantage of the stealth system. NPC Questlines spanning multiple Levels, even colliding with other NPC Questlines, making you choose who you want to favor in those deciding moments.
             </span>
           </p>
