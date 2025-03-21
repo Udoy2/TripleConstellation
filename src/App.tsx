880f7ef25a58bd853a4c5d2fb09ee3c48652ae14
udoy <udoyrahman983@gmail.com>
2025-03-21 22:38:13 +0600

    feat: implement loading button in Loader component and update App to manage button click state

--- src/App.tsx
+++ src/App.tsx
@@ -6,31 +6,34 @@ import Navbar from './components/Navbar';
 
 function App() {
   const [loading, setLoading] = useState(true);
+  const [enterButtonClicked, setEnterbuttonClicked] = useState(false);  
 
-  // useEffect(() => {
-  //   const handleLoad = () => {
-  //     setLoading(false);
-  //   };
-
-  //   window.addEventListener('load', handleLoad);
-
-  //   return () => {
-  //     window.removeEventListener('load', handleLoad);
-  //   };
-  // }, []);
   useEffect(() => {
-    setLoading(true);
-
-    setTimeout(() => {
+    const handleLoad = () => {
       setLoading(false);
-    }
-    , 3000);
+    };
+
+    window.addEventListener('load', handleLoad);
+
+    return () => {
+      window.removeEventListener('load', handleLoad);
+    };
   }, []);
+  // useEffect(() => {
+  //   setLoading(true);
+
+  //   setTimeout(() => {
+  //     setLoading(false);
+  //   }
+  //   , 3000);
+  // }, []);
 
   return (
     <>
       <Navbar />
-      {loading ? <Loader /> : <Home />}
+      {!enterButtonClicked ? <Loader  
+              loading={loading} 
+             setEnterButtonClicked={setEnterbuttonClicked}/> : <Home />}
     </>
   );
 }
--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,12 +1,30 @@
-import { loaderVideo } from '../assets/Assets';
+import { loaderVideo } from "../assets/Assets";
 
-const Loader = () => {
+interface LoaderProps {
+  loading: boolean;
+  setEnterButtonClicked: (value: boolean) => void;
+}
+
+const Loader = ({ loading, setEnterButtonClicked }: LoaderProps) => {
   return (
-    <div className="loader fixed top-0 left-0 z-40 h-screen w-screen">
+    <div className="loader fixed top-0 left-0 z-40 h-screen w-screen flex items-center justify-center">
       <video className="h-full w-full object-cover" autoPlay loop muted>
         <source src={loaderVideo} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
+      <button
+        className={`
+          fixed top-[50%] left-[50%] z-50 -translate-x-[50%]
+          -translate-y-[50%] cursor-pointer bg-[#604410]
+          px-5 py-3 text-white transition-all hover:scale-102
+          hover:drop-shadow-[0_0_10px_rgba(255,237,212,0.5)] text-2xl
+          ${loading ? 'opacity-50 cursor-not-allowed' : 'hover:scale-105'}
+        `}
+        disabled={loading}
+        onClick={() => setEnterButtonClicked(true)}
+      >
+        {loading ? 'Loading...' : 'Click'}
+      </button>
     </div>
   );
 };
